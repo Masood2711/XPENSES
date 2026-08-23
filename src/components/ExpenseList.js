16038dc628e361e5533f77664693e6f6b2e63a68
Masood2711 <107754125+Masood2711@users.noreply.github.com>
2026-08-23 18:17:26 +0530

    Upload XPENSES project

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+function ExpenseList({expenses,onDelete,onEdit}){
+    return(
+        <section className="expense-list-section">
+            <div className="section-header">
+                <h2>Recent Transactions</h2>
+                {expenses.length==0?(
+                    <div className="empty-expenses">
+                        <p>No transactions!</p>
+                        <p>Add an expense to see it here</p>
+                    </div>
+                ):(
+                    <div className="expense-items">
+                        {expenses.map((expense)=>(
+                            <ExpenseItem key={expense.id} expense={expense} onEdit={onEdit} onDelete={onDelete}/>
+                        ))}
+                    </div>
+                )}
+            </div>
+
+        </section>
+    )
+}
+
+export default ExpenseList;
